b8afd3e85446d4000b3ac270addbc46b5a088098
Stefan Bühler <stbuehler@web.de>
2017-06-19 20:29:41 +0200

    restrict linking avahi compat to unix without macos/ios

--- build.rs
+++ build.rs
@@ -1,10 +1,23 @@
 extern crate pkg_config;
 
-fn get_target() -> String {
-	std::env::var("TARGET").unwrap()
+use std::env::var_os;
+
+fn cfg_family(family: &str) -> bool {
+	var_os("CARGO_CFG_TARGET_FAMILY").unwrap() == *family
 }
-fn main() {
-	if !get_target().contains("darwin") {
-		pkg_config::find_library("avahi-compat-libdns_sd").unwrap();
+
+fn cfg_os(family: &str) -> bool {
+	var_os("CARGO_CFG_TARGET_OS").unwrap() == *family
+}
+
+fn find_avahi_compat_dns_sd() {
+	// on unix but not darwin link avahi compat
+	if cfg_family("unix")
+	&& !(cfg_os("macos") || cfg_os("ios")) {
+		pkg_config::probe_library("avahi-compat-libdns_sd").unwrap();
 	}
+}
+
+fn main() {
+	find_avahi_compat_dns_sd();
 }
