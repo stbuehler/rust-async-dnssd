8d6a4ee2d007c942b209922984abd8d5e0f48b03
Stefan Bühler <source@stbuehler.de>
2021-11-26 18:56:07 +0100

    support cross-compiling windows by using proper path separators

--- build.rs
+++ build.rs
@@ -31,7 +31,7 @@ fn find_windows_dns_sd() {
 		};
 
 		match var("BONJOUR_SDK_HOME") {
-			Ok(path) => println!("cargo:rustc-link-search=native={}\\Lib\\{}", path, platform),
+			Ok(path) => println!("cargo:rustc-link-search=native={}{sep}Lib{sep}{}", path, platform, sep=std::path::MAIN_SEPARATOR),
 			Err(e) => panic!("Can't find Bonjour SDK (download from https://developer.apple.com/opensource/) at BONJOUR_SDK_HOME: {}", e),
 		}
 		println!("cargo:rustc-link-lib=dnssd");
